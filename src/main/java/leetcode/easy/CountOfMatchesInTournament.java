92cf3fbca273892abc568b202fdcca16e117ad24
장총명 <jang.cm@kstc.co.kr>
2021-03-11 20:55:02 +0900

    first commit

---
+++ src/main/java/leetcode/easy/CountOfMatchesInTournament.java
@@ -0,0 +1,32 @@
+package leetcode.easy;
+
+/**
+ * {@link https://leetcode.com/problems/count-of-matches-in-tournament/}
+ * @author jang.cm
+ */
+
+public class CountOfMatchesInTournament {
+
+    public static void main(String[] args) {
+
+        System.out.println("tesggggggggggt");
+
+    }
+
+
+
+}
+
+class Solution1 {
+    public int numberOfMatches(int n) {
+
+        return n-1;
+    }
+}
+
+class Solution2 {
+    public int numberOfMatches(int n) {
+
+        return n-1;
+    }
+}
